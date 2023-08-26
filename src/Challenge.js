682cd9183605d478f7216a8f70ac366634dfd0c6
Krunal Vatvani <krunalvatvani2424@gmail.com>
2023-08-26 15:09:01 +0530

    first commit

---
+++ src/Challenge.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Challenge(){
+    return (
+
+        <h1>HEllo</h1>
+    );
+
+}
+
+export default Challenge;
---
+++ src/index.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+
+// import ReactDOM from 'react-dom/client';
+import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// ReactDOM.render(<h1>Hello</h1>,document.getElementById('root')); //single render jsx
+var name = "Krunal Vatvani";
+
+const image1 = "https://picsum.photos/200/300";
+const image2 = "https://picsum.photos/250/300";
+const image3 = "https://picsum.photos/250/300";
+const link = "http://localhost:3000/";
+
+ReactDOM.render(<h1> My Name is:-  { name }</h1>,document.getElementById('krunal'));
+
+// Challenage Number 1
+ReactDOM.render(
+    <>
+        <h1> Netflic Pick </h1>
+        <p> Name of Netflic Pick </p>
+        <ol>
+            <li>The Monkey King</li>
+            <li>The Pope's Exorcist</li>
+            <li>Half Brothers</li>
+            <li>Hidden Strike</li>
+            <li>What Men Want</li>
+        </ol>
+    </>
+,document.getElementById('challenge'));
+
+
+//Challenge 2 
+
+ReactDOM.render(
+<> 
+<h1>{`Helo, My Name is ${name}`}</h1>
+<p> Current Date is:- { new Date().toLocaleDateString()}</p>
+<p> Current Time is:- { new Date().toLocaleTimeString() }</p>
+</>,document.getElementById('challenge-second'));
+
+
+ReactDOM.render(
+<>
+<h1 className='heading'>Image Gallery</h1>
+<div className='img_div'>
+
+    <a href={link}>
+        <img src= {image1} alt="randomImage" />
+    </a>
+    <img src= {image2} alt="randomImage" />
+    <img src= {image3} alt="randomImage" />
+
+
+</div>
+
+
+</>,document.getElementById('image-gallery'));
+
+
+
+// ReactDOM.render([
+//     <h1>Hello</h1>,
+//     <p>this is paragraph tag</p>,
+//     ],
+//     document.getElementById('root')
+//     ); //single render jsx
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
