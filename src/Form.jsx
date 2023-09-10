76c0bcb741aad28f029654e53fa25686b4d6dd26
Krunal Vatvani <krunalvatvani2424@gmail.com>
2023-09-10 20:19:20 +0530

    learn form submit in sort code

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,42 +2,44 @@ import React,{useState} from "react";
 
 function Form () {
 
-    const [name,setName] = useState("");
-    
-    const [fullName,setFullName] = useState("");
+    const [fullName,setFullName] = useState({
+        first_name : "",
+        email : "",
+    });
 
-    const [email,setEmail] = useState("");
+    const getInputValue = (event)=> {
+        const {name ,value} = event.target;
 
-    const [newEmail,setNewEmail] = useState("");
-
-    const setInputFormName = (event)=> {
-        setName(event.target.value);
-    }
-
-    const setInputFormEmail = (event)=> {
-        setEmail(event.target.value);
+        setFullName((previousValue) => {
+            console.log(previousValue);
+            return{
+                ...previousValue,
+                [name]  : value
+            }
+        });
     }
 
     const onSubmitForm = (event) => {
         event.preventDefault();
         
-        setFullName(name);
+        console.log("Form Submitted successfully");
 
-        setNewEmail(email);
+        // alert("Form Submitted Successfully");
+        // setFullName(name);
     }
     
 
     return(
         <>
-            <h1>{fullName} {newEmail}</h1>
+            <h1> Your Name Is:- {fullName.first_name} Your Email Is:- {fullName.email} </h1>
             <form onSubmit={onSubmitForm}>
-                <div className="form-group">
-                    <label>Email address:-</label>
-                    <input type="email" className="form-control" placeholder="Enter email" value={email} onChange={setInputFormEmail}/>
-                </div>
                 <div className="form-group mt-2">
                     <label>Name:-</label>
-                    <input type="text" className="form-control" placeholder="Enter Name" value={name} onChange={setInputFormName} />
+                    <input type="text" name="first_name" className="form-control" placeholder="Enter Name" value={fullName.first_name} onChange={getInputValue} />
+                </div>
+                <div className="form-group">
+                    <label>Email address:-</label>
+                    <input type="email" name="email" className="form-control" placeholder="Enter email" value={fullName.email} onChange={getInputValue}/>
                 </div>
                 <button type="submit" className="btn btn-primary mt-2">Submit</button>
             </form>
