e618f81171082ccce0b7fc1abb980caba99acdaf
Krunal Vatvani <krunalvatvani2424@gmail.com>
2023-10-29 12:51:09 +0530

    react routing learning

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -18,52 +18,52 @@ const Nav = () => {
           >
         <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="collapse menu-list navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item active">
-                <Link className="nav-link" to="/">Home</Link>
+                <NavLink exact activeClassName="active" className="nav-link" to="/">Home</NavLink>
               </li>
               <li className="nav-item active">
-                <Link className="nav-link" to="/challenge-one">Challenge One</Link>
+                <NavLink exact activeClassName="active" className="nav-link" to="/challenge-one">Challenge One</NavLink>
               </li>
               <li className="nav-item active">
-                <Link className="nav-link" to="/challenge-two">Challenge Two</Link>
+                <NavLink exact activeClassName="active" className="nav-link" to="/challenge-two">Challenge Two</NavLink>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/challenge-three">Challenge Three</Link>
+              <li className="nav-item active">
+                <NavLink exact activeClassName="active" className="nav-link" to="/challenge-three">Challenge Three</NavLink>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/image-gallery">Image Gallery</Link>
+              <li className="nav-item active">
+                <NavLink exact activeClassName="active" className="nav-link" to="/image-gallery">Image Gallery</NavLink>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/use-state">useState</Link>
+              <li className="nav-item active">
+                <NavLink exact activeClassName="active" className="nav-link" to="/use-state">useState</NavLink>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/todo-list">ToDo List</Link>
+              <li className="nav-item active">
+                <NavLink exact activeClassName="active" className="nav-link" to="/todo-list">ToDo List</NavLink>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/todo-list-toggle">Show Data Based On Toggle</Link>
+              <li className="nav-item active">
+                <NavLink exact activeClassName="active" className="nav-link" to="/todo-list-toggle">Show Data Based On Toggle</NavLink>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/props">Props</Link>
+              <li className="nav-item active">
+                <NavLink exact activeClassName="active" className="nav-link" to="/props">Props</NavLink>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/user-props">User Props</Link>
+              <li className="nav-item active">
+                <NavLink exact activeClassName="active" className="nav-link" to="/user-props">User Props</NavLink>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/form">Form</Link>
+              <li className="nav-item active">
+                <NavLink exact activeClassName="active" className="nav-link" to="/form">Form</NavLink>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/form-two">Form Two</Link>
+              <li className="nav-item active">
+                <NavLink exact activeClassName="active" className="nav-link" to="/form-two">Form Two</NavLink>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/form-three">Form Three</Link>
+              <li className="nav-item active">
+                <NavLink exact activeClassName="active" className="nav-link" to="/form-three">Form Three</NavLink>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/name">Name</Link>
+              <li className="nav-item active">
+                <NavLink exact activeClassName="active" className="nav-link" to="/name">Name</NavLink>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/user-context">UseContext</Link>
+              <li className="nav-item active">
+                <NavLink exact activeClassName="active" className="nav-link" to="/user-context">UseContext</NavLink>
               </li>
             </ul>
           </div>
